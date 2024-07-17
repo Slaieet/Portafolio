651ff8d6493e7f6425cc4052096182a1c503b4e2
Slaieet <sl4ieet@gmail.com>
2024-07-17 11:28:00 -0600

    fist commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundColor: {
+				main: '#11071f'
+			},
+			textColor: {
+				main: '#fafafa'
+			}
+		},
+	},
+	plugins: [],
+}
