11636d6561c66412af06d2e887e8b65026a719e5
Slaieet <sl4ieet@gmail.com>
2024-07-20 21:11:45 -0600

    add technologies section

---
+++ src/utils/technologies.ts
@@ -0,0 +1,30 @@
+export const technologies = [
+  {
+    name: 'HTML',
+    url: '/icons/html.svg'
+  },
+  {
+    name: 'CSS',
+    url: '/icons/css.svg'
+  },
+  {
+    name: 'JavaScript',
+    url: '/icons/js.svg'
+  },
+  {
+    name: 'React',
+    url: '/icons/react.svg'
+  },
+  {
+    name: 'TypeScript',
+    url: '/icons/typescript.svg'
+  },
+  {
+    name: 'Astro',
+    url: '/icons/astro.svg'
+  },
+  {
+    name: 'Vite',
+    url: '/icons/vite.svg'
+  }
+]
