555ff209f22b1afc93a3919da4572b17699554aa
Slaieet <sl4ieet@gmail.com>
2024-07-22 16:17:29 -0600

    add info real in contact section

---
+++ src/utils/icons.ts
@@ -0,0 +1,52 @@
+export const technologies = [
+  {
+    name: 'HTML',
+    url: '/icons/html.svg'
+  },
+  {
+    name: 'CSS',
+    url: '/icons/css.svg'
+  },
+  {
+    name: 'JavaScript',
+    url: '/icons/js.svg'
+  },
+  {
+    name: 'TypeScript',
+    url: '/icons/typescript.svg'
+  },
+  {
+    name: 'React',
+    url: '/icons/react.svg'
+  },
+  {
+    name: 'Tailwind',
+    url: '/icons/tailwind.svg'
+  },
+  {
+    name: 'Vite',
+    url: '/icons/vite.svg'
+  },
+  {
+    name: 'Astro',
+    url: '/icons/astro.svg'
+  }
+]
+
+export const socialMediaIcons = [
+  {
+    name: 'LinkedIn',
+    imgUrl: '/icons/social-media/linkedin.svg',
+    url: 'https://www.linkedin.com/in/angel-jasso-046b74292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
+  },
+  {
+    name: 'Instagram',
+    imgUrl: '/icons/social-media/instagram.svg',
+    url: 'https://instagram.com/ang_rtv'
+  },
+  {
+    name: 'GitHub',
+    imgUrl: '/icons/social-media/github.svg',
+    url: 'https://github.com/slaieet'
+  }
+]
