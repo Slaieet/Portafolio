ce4b1f8396fccfd49f527e88216d94e91d3d0fd5
Slaieet <sl4ieet@gmail.com>
2024-09-09 17:20:57 -0600

    remove instagram from contact icons

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -40,11 +40,6 @@ export const socialMediaIcons = [
     url: 'https://www.linkedin.com/in/angel-jasso-046b74292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
   },
   {
-    name: 'Instagram',
-    imgUrl: '/icons/social-media/instagram.svg',
-    url: 'https://instagram.com/ang_rtv'
-  },
-  {
     name: 'GitHub',
     imgUrl: '/icons/social-media/github.svg',
     url: 'https://github.com/slaieet'
